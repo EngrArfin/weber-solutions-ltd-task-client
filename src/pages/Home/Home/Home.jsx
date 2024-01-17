7390be522d507311ffa170ce38e024ac06fb5c28
EngrArfin <arfin.cse.green.edu.bd@gmail.com@>
2024-01-18 00:32:02 +0600

    home page update

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import NavBar from '../../NavBar/NavBar';
-import UserList from '../Server/UserList/UserList';
 
 const Home = () => {
     return (
         <div>
-            
             <NavBar></NavBar>
-            
-            
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content text-center">
+                    <div className="max-w-md">
+                        <h1 className="text-4xl font-bold">WEBYAPAR SOLUTIONS PVT. LTD.
+</h1>
+                        <p className="py-6">Full Stack Development </p>
+                        <button className="btn btn-primary">Get Internship</button>
+                    </div>
+                </div>
+            </div>
+
+
         </div>
     );
 };
--- src/pages/NavBar/NavBar.jsx
+++ src/pages/NavBar/NavBar.jsx
@@ -5,9 +5,9 @@ const NavBar = () => {
 
   return (
 
-    <div className='ml-40 mt-4'>
+    <div className='ml-30 mt-1'>
       <Link  className="btn btn-outline btn-warning">Home</Link >
-      <Link to ='/userlogin' className="btn btn-outline btn-warning m-5">Login User</Link >
+      <Link to ='/userlogin' className="btn btn-outline btn-warning m-2">Login User</Link >
       <Link to='/adminlogin'  className="btn btn-outline btn-warning">Login Admin</Link >
     </div>
 
